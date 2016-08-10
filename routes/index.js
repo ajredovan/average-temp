2623ff345db5cc08bc9e62bc00b8597c294b5163
Jinno Redovan <jredovan.dev@gmail.com>
2016-08-10 16:34:22 -0500

    changing var to let

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-let express = require('express');
-let router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const got = require("got");
 const forecastIo = require('../ForecastIO.js');
